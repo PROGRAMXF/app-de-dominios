42324b04fec85bd1122b649f5de626b400a3868b
Fernando <fh1273509@gmail.com>
2023-03-24 12:25:08 -0300

    creacion de la carpeta views con sus archivos

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //definimos nuestro motor de plantillas
-app.set('view engine', 'ejs');
-
-
-
+app.set("view engine", "ejs");
 
 //invocamos los metodos de express
 
-app.listen(5000, ()=>{
-    console.log('Server corriendo');
+app.listen(5000, () => {
+  console.log("Server corriendo");
 });
 
 //colocamos nodemon app para cuando grabemos los cambios el server corra autmaticamente
