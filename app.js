4bfc4c91db73b5aa4ebcbc908a4df452b6e98863
Fernando <fh1273509@gmail.com>
2023-03-24 12:21:22 -0300

    definimos el motor de plantillas

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+//definimos nuestro motor de plantillas
+app.set('view engine', 'ejs');
+
+
+
+
 //invocamos los metodos de express
 
 app.listen(5000, ()=>{
     console.log('Server corriendo');
 });
 
+//colocamos nodemon app para cuando grabemos los cambios el server corra autmaticamente
