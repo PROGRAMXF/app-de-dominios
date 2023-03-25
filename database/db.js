72174784a05aba43710ff6804dab0f329960590c
Fernando <fh1273509@gmail.com>
2023-03-25 20:33:12 -0300

    configuracion router.js

--- database/db.js
+++ database/db.js
@@ -7,3 +7,12 @@ const conexion = mysql.createConnection({
     password: '',
     database: 'crud_nodejs_db'
 });
+
+conexion.connect((error)=>{
+    if(error){
+        console.error('El error de coneccion es: ' +error);
+        return
+    }
+    console.log('Conectado a la bd de Mysql');
+});
+module.exports = conexion;
--- router.js
+++ router.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/contacto', (req, res)=>{
-    res.send('CONTACTO');
+const conexion  = require('./database/db');
+
+router.get('/', (req, res)=>{
+    conexion.query('SELECT * FROM users', (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    })
+
 });
 
 module.exports = router;
