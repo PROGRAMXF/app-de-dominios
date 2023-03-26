1737b584754b7fcf19079eb68399d42e217078f2
Fernando <fh1273509@gmail.com>
2023-03-26 18:51:46 -0300

    plantilla con bootstrap

--- router.js
+++ router.js
@@ -4,13 +4,16 @@ const router = express.Router();
 const conexion  = require('./database/db');
 
 router.get('/', (req, res)=>{
-    conexion.query('SELECT * FROM users', (error, results)=>{
+
+        res.render('index');
+
+    /*conexion.query('SELECT * FROM users', (error, results)=>{
         if(error){
             throw error;
         }else{
             res.send(results);
         }
-    })
+    })*/
 
 });
 
